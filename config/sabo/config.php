d767db82e8a2dc4df38ea192bf937c0a15d4f985
Bathily Yahaya <developpeur.yb@gmail.com>
2023-03-17 15:26:51 +0100

    début d'implémentation des fonctionnalités mysql

--- config/sabo/config.php
+++ config/sabo/config.php
@@ -5,4 +5,4 @@ use Sabo\Config\SaboConfigAttributes;
 
 SaboConfig::setBoolConfig(SaboConfigAttributes::DEBUG_MODE,true);
 SaboConfig::setBoolConfig(SaboConfigAttributes::INIT_WITH_DATABASE_CONNEXION,true);
-SaboConfig::setStrConfig(SaboConfigAttributes::ENV_FILE_TYPE,".env");
+// SaboConfig::setStrConfig(SaboConfigAttributes::ENV_FILE_TYPE,".env");
---
+++ sabo/model/system/mysql/SaboMysql.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Sabo\Model\System\Mysql;
+
+use PDO;
+use Exception;
+use Sabo\Config\EnvConfig;
+use Sabo\Config\SaboConfig;
+use Sabo\Config\SaboConfigAttributes;
+
+/**
+ * représente une base de donnée MYSQL
+ */
+trait SaboMysql{
+    /**
+     * identifiants de connexion à la base de données
+     */
+    private static array $databaseConfig;
+
+    /**
+     * connexion partagé entre les modèles
+     */
+    private static ?PDO $sharedCon = null;
+
+    /**
+     * connexion du modèle
+     */
+    private ?PDO $myCon = null;
+
+    /**
+     * @return PDO|null la connexion
+     */
+    public function getMyCon():?PDO{
+        return $this->myCon;
+    }
+
+    /**
+     * initialise les modèles
+     */
+    public static function initModel():bool{
+        $configEnv = EnvConfig::getConfigEnv();
+
+        // récupération des identifiants de base de données
+        if(EnvConfig::getIsJsonEnv() ){
+            if(!isset($configEnv["database"]["host"],$configEnv["database"]["user"],$configEnv["database"]["password"],$configEnv["database"]["name"]) ) throw new Exception("Missed a database key in configuration");
+
+            self::$databaseConfig = [
+                "host" => $configEnv["database"]["host"],
+                "user" => $configEnv["database"]["user"],
+                "password" => $configEnv["database"]["password"],
+                "name" => $configEnv["database"]["name"]
+            ];
+
+            if(isset($configEnv["database"]["port"]) ) self::$databaseConfig["port"] = $configEnv["database"]["port"];
+        }
+        else{
+            if(!isset($configEnv["DB_HOST"],$configEnv["DB_USER"],$configEnv["DB_PASSWORD"],$configEnv["DB_NAME"]) ) throw new Exception("Missed a database key in configuration");
+
+            self::$databaseConfig = [
+                "host" => $configEnv["DB_HOST"],
+                "user" => $configEnv["DB_USER"],
+                "password" => $configEnv["DB_PASSWORD"],
+                "name" => $configEnv["DB_NAME"]
+            ];
+
+            if(isset($configEnv["DB_PORT"]) ) self::$databaseConfig["port"] = $configEnv["DB_PORT"];
+        }
+
+        self::$sharedCon = self::getNewCon();
+
+        return self::$sharedCon != null;
+    }
+
+    /**
+     * @return PDO|null une nouvelle connexion à la base de données
+     */
+    private static function getNewCon():?PDO{
+        try{
+            list("host" => $host,"user" => $user,"name" => $name,"password" => $password) = self::$databaseConfig;
+
+            $pdo = new PDO("mysql:host={$host};dbname={$name};charset=UTF8",$user,$password,[
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+
+            $pdo->exec("set names utf8");
+
+            return $pdo;
+        }
+        catch(Exception $e){
+            if(SaboConfig::getBoolConfig(SaboConfigAttributes::DEBUG_MODE) ) throw $e;
+
+            return null;
+        }
+
+    }
+
+    private static function getSharedCon():?PDO{
+        return self::$sharedCon;
+    }
+}

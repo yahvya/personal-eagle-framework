97c9bcbbc4415a6fcf73593e58d7c01172d3617c
Bathily Yahaya <developpeur.yb@gmail.com>
2023-03-26 13:37:21 +0200

    correction du htaccess

--- config/routes/routes.php
+++ config/routes/routes.php
@@ -1,11 +1,10 @@
 <?php
 
+use Controller\HomeController;
 use Sabo\Sabo\Route;
 
 return Route::generateFrom([
-    Route::group("/",[
-        Route::get("",function():void{
-            echo "je suis bien ici";            
-        },"Home@home-page",accessConds:[fn():bool => true])
-    ],[fn():bool => true,fn():bool => true])
+    Route::get("/",function():void{
+        echo "page d'accueil";
+    },"Home:home")
 ]);

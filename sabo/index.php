0722029251e55da6c46b59a8b848c36f7b32df0a
Bathily Yahaya <developpeur.yb@gmail.com>
2023-04-12 18:46:39 +0200

    correction de l'ordre d'inclusion autoloader

--- sabo/index.php
+++ sabo/index.php
@@ -10,14 +10,14 @@ define("ROOT",__DIR__ . "\\..\\");
 
 require_once(__DIR__ . "/vendor/autoload.php");
 
+// inclusion de l'autoloader utilisateur
+if(FileHelper::fileExist(PathConfig::USER_AUTOLOAD_FILEPATH->value) ) Helper::require(PathConfig::USER_AUTOLOAD_FILEPATH->value);
+
 // définition des configurations par défaut
 SaboConfig::setDefaultConfigurations();
 
 // inclusion des configurations utilisateur
 Helper::require(PathConfig::SABO_CONFIG_FILEPATH->value);
-
-// inclusion de l'autoloader utilisateur
-if(FileHelper::fileExist(PathConfig::USER_AUTOLOAD_FILEPATH->value) ) Helper::require(PathConfig::USER_AUTOLOAD_FILEPATH->value);
 
 // démarrage du site
 Router::initWebsite();

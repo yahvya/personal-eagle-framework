7b9f938add952757b32a5688e5df00e28418aa47
Bathily Yahaya <developpeur.yb@gmail.com>
2024-03-09 02:20:51 +0100

    ajout de la page de maintenance par défaut fonction de stockage à faire ajout de la page d'accueil du framework

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,3 +1,15 @@
 <?php
 
 // routes web
+
+use SaboCore\Routing\Response\BladeResponse;
+use SaboCore\Routing\Routes\Route;
+use SaboCore\Routing\Routes\RouteManager;
+
+RouteManager::registerRoute(
+    Route::get(
+        link: "/",
+        toExecute: fn():BladeResponse => new BladeResponse("sabo",["websiteLink" => "https://github.com/yahvya/sabo-final"]),
+        routeName: "sabo"
+    )
+);

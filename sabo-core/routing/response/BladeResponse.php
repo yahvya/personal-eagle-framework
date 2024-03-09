68bd0d74ac87db1bdc99cb931e8fcc4ebbda70e1
Bathily Yahaya <developpeur.yb@gmail.com>
2024-03-09 02:01:19 +0100

    ajout de la page de maintenance par défaut fonction de stockage à faire

---
+++ sabo-core/routing/response/BladeResponse.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace SaboCore\Routing\Response;
+
+use Illuminate\Container\Container;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\View\Compilers\BladeCompiler;
+use Illuminate\View\Engines\CompilerEngine;
+use Illuminate\View\Engines\EngineResolver;
+use Illuminate\View\Engines\PhpEngine;
+use Illuminate\View\Factory;
+use Illuminate\View\FileViewFinder;
+use Throwable;
+
+/**
+ * @brief Réponse fichier blade
+ * @author yahaya bathily https://github.com/yahvya
+ */
+class BladeResponse extends HtmlResponse{
+    /**
+     * @param string $pathFromViews chemin à partir du dossier des vues
+     * @param array $datas données de la vue
+     */
+    public function __construct(string $pathFromViews,array $datas = []){
+        try{
+            $factory = self::newFactory([APP_CONFIG->getConfig("ROOT") . "/src/views/"]);
+
+            parent::__construct($factory->make($pathFromViews,$datas)->render());
+        }
+        catch(Throwable){
+            parent::__construct("Veuillez rechargez la page");
+        }
+    }
+
+    /**
+     * @param array $viewsPath chemin du dossier des vues
+     * @return Factory|null le factory crée
+     */
+    public static function newFactory(array $viewsPath):Factory|null{
+        try{
+            $pathToCompiledTemplates = APP_CONFIG->getConfig("ROOT") . "/sabo-core/views/blade/compiled";
+            $filesystem = new Filesystem;
+            $eventDispatcher = new Dispatcher(new Container);
+            $viewResolver = new EngineResolver;
+            $bladeCompiler = new BladeCompiler($filesystem, $pathToCompiledTemplates);
+            $viewResolver->register("blade", function () use ($bladeCompiler) {
+                return new CompilerEngine($bladeCompiler);
+            });
+            $viewResolver->register("php", function () use($filesystem) {
+                return new PhpEngine($filesystem);
+            });
+
+            $viewFinder = new FileViewFinder($filesystem, $viewsPath);
+
+            return new Factory($viewResolver, $viewFinder, $eventDispatcher);
+        }
+        catch(Throwable){
+            return null;
+        }
+    }
+}
--- src/controllers/DefaultMaintenanceController.php
+++ src/controllers/DefaultMaintenanceController.php
@@ -5,17 +5,20 @@ namespace Controllers;
 use Override;
 use SaboCore\Controller\MaintenanceController;
 use SaboCore\Routing\Request\Request;
-use SaboCore\Routing\Response\HtmlResponse;
+use SaboCore\Routing\Response\BladeResponse;
 use SaboCore\Routing\Response\Response;
 
 class DefaultMaintenanceController extends MaintenanceController{
     #[Override]
     public function showMaintenancePage(string $secretLink): Response{
-        return new HtmlResponse($secretLink);
+        return new BladeResponse("maintenance/authentication",[
+            "secretLink" => $secretLink
+        ]);
     }
 
     #[Override]
     public function verifyLogin(Request $request): bool{
+        debugDie("verification");
         return true;
     }
 }

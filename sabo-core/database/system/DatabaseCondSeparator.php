d1539f4ca098c92af825c90ce36f757196f0b60c
Bathily Yahaya <developpeur.yb@gmail.com>
2024-04-15 17:44:42 +0200

    Conversion en class des énumérations de comparaison et de séparation

--- sabo-core/database/system/DatabaseCondSeparator.php
+++ sabo-core/database/system/DatabaseCondSeparator.php
@@ -6,6 +6,23 @@ namespace SaboCore\Database\System;
  * @brief Représentation de la séparation entre deux conditions
  * @author yahaya bathily https://github.com/yahvya
  */
-enum DatabaseCondSeparator:string{
+class DatabaseCondSeparator{
+    /**
+     * @var string Séparateur
+     */
+    protected string $separator;
 
+    /**
+     * @param string $separator Séparateur
+     */
+    protected function __construct(string $separator){
+        $this->separator = $separator;
+    }
+
+    /**
+     * @return string Séparateur
+     */
+    public function getSeparator(): string{
+        return $this->separator;
+    }
 }

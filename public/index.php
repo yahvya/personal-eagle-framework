3cc140c30a0a07c2e5ca30177605d27348080281
Yahaya <developpeur.yb@gmail.com>
2024-09-03 20:29:12 +0200

    add user composer files start defining entrypoint script

--- public/index.php
+++ public/index.php
@@ -4,3 +4,24 @@
  * @brief Sabo framework entrypoint
  * @author yahaya https://github.com/yahvya
  */
+
+# --------------------------------------------------------------------
+# define consts
+# --------------------------------------------------------------------
+
+/**
+ * @const app root path
+ * @attention without / at the end
+ */
+const APP_ROOT = __DIR__ . "/..";
+
+# --------------------------------------------------------------------
+# loading autoloader
+# --------------------------------------------------------------------
+
+require_once APP_ROOT . "/sabo-core/vendor/autoload.php";
+require_once APP_ROOT . "/vendor/autoload.php";
+
+# --------------------------------------------------------------------
+# launch app
+# --------------------------------------------------------------------

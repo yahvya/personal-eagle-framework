5faf9702168edc1b3189868e4eeed34056c6c50e
Yahaya <developpeur.yb@gmail.com>
2024-09-03 21:09:53 +0200

    add htaccess redirection

--- public/index.php
+++ public/index.php
@@ -16,11 +16,16 @@
 const APP_ROOT = __DIR__ . "/..";
 
 # --------------------------------------------------------------------
-# loading autoloader
+# load autoloader
 # --------------------------------------------------------------------
 
 require_once APP_ROOT . "/sabo-core/vendor/autoload.php";
 require_once APP_ROOT . "/vendor/autoload.php";
+
+# --------------------------------------------------------------------
+# load custom scripts
+# --------------------------------------------------------------------
+require_once APP_ROOT . "/sabo-core/custom-scripts/global-functions.php";
 
 # --------------------------------------------------------------------
 # launch app
---
+++ sabo-core/custom-scripts/global-functions.php
@@ -0,0 +1,5 @@
+<?php
+
+/**
+ * @brief Available global functions from the framework
+ */
